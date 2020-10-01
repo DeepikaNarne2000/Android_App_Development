90c4f5968f024fe04477c2a7232bb9519d76399d
Shashank Kumar Dahiya <63150026+sudeep152@users.noreply.github.com>
2020-10-01 17:37:12 +0530

    Update SplashScreen.java

--- Blood_Bank/app/src/main/java/com/example/blood_bank/Activities/SplashScreen.java
+++ Blood_Bank/app/src/main/java/com/example/blood_bank/Activities/SplashScreen.java
@@ -12,5 +12,6 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+Toast.makeText(getApplicationContext(),"Please Wait....",Toast.LENGTH_SHORT).show();  
     }
 }
